858e7cf7625a503dfe7b8e7722405524324c1b65
Fioletowy <118393950+FioletowyGtag@users.noreply.github.com>
2024-07-04 18:07:44 +0200

    Create quotemakersourcecode.py
    source code

---
+++ quotemakersourcecode.py
@@ -0,0 +1,11 @@
+print("\t\t Quote Maker")
+print("\t      By FioletowyGtag")
+
+
+
+quote = input("type the quote here : ")
+whosaysthat = input("said by - : ")
+space = "| said by : |"
+generatatedquote = (quote + "\'")
+
+print(quote + space + whosaysthat)
